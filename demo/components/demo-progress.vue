bfcfccd4ec6587b3a6910e0b777c6d706e0feece
martsforever <martsforever@aliyun.com>
2019-02-08 22:12:56 +0800

    almost complete progress component

--- demo/components/demo-progress.vue
+++ demo/components/demo-progress.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="demo-progress">
         <demo-row>
-            <link-progress :value="val1"/>
+            <link-progress :value="val1" ref="progress"/>
             <div>
                 <link-button @click="val1+=0.1">add</link-button>
                 <link-button @click="val1-=0.1">subtract</link-button>
+                <link-button @click="$refs.progress.changeStatus('error')">error</link-button>
+                <link-button @click="$refs.progress.changeStatus('normal')">normal</link-button>
             </div>
         </demo-row>
     </div>
--- src/components/progress/pl-progress.vue
+++ src/components/progress/pl-progress.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="pl-progress">
         <div class="pl-progress-bar">
-            <div class="pl-progress-bar-percent" :style="percentStyles" ref="percent"></div>
+            <div class="pl-progress-bar-percent" :style="percentStyles" :class="percentClasses" ref="percent"></div>
             <div class="pl-progress-bar-active" :style="activeStyles"></div>
         </div>
-        <div>{{(currentValue*100).toFixed(2)}}%</div>
+        <div class="pl-progress-label">{{showLabel}}</div>
     </div>
 </template>
 
@@ -16,6 +16,7 @@
         mixins: [ValueMixin],
         props: {
             value: {type: Number, default: 0},
+            status: {type: String, default: 'normal'},
         },
         watch: {
             currentValue: {
@@ -23,9 +24,20 @@
                 handler(val) {
                     this.$nextTick(() => this.activeTotalWidth = this.$refs.percent.offsetWidth)
                 },
-            }
+            },
+            status(val) {
+                if (val !== this.p_status) this.p_status = val
+            },
+            p_status(val) {
+                this.$emit('update:status', val)
+            },
         },
         computed: {
+            percentClasses() {
+                return [
+                    `pl-progress-bar-percent-${this.finalStatus}`
+                ]
+            },
             percentStyles() {
                 return {
                     width: `${this.currentValue * 100}%`
@@ -37,12 +49,26 @@
                     opacity: 1 - (this.activeWidth / this.activeTotalWidth) + 0.1
                 }
             },
+            isComplete() {
+                return Math.abs(this.currentValue - 1) < 0.01
+            },
+            showLabel() {
+                if (this.finalStatus === 'success') return '成功'
+                if (this.finalStatus === 'error') return '失败'
+                return (this.currentValue * 100).toFixed(2) + '%'
+            },
+            finalStatus() {
+                if (this.$plain.$utils.oneOf(this.p_status, ['success', 'error'])) return this.p_status
+                if (this.isComplete) return 'success'
+                return 'normal'
+            },
         },
         data() {
             return {
+                p_status: this.status,
                 activeTotalWidth: 0,
                 activeWidth: 100,
-                timer: null
+                timer: null,
             }
         },
         mounted() {
@@ -56,7 +82,10 @@
                     if (this.activeWidth > this.activeTotalWidth) {
                         this.activeWidth = 0
                     }
-                }, 5)
+                }, 10)
+            },
+            changeStatus(status) {
+                this.p_status = status
             },
             clearTimer() {
                 if (!!this.timer) {
@@ -81,12 +110,20 @@
             background-color: $color-info;
             border-radius: 9px;
             height: 100%;
-            margin-right: 12px;
             position: relative;
             .pl-progress-bar-percent {
                 height: 100%;
-                background-color: $color-primary;
                 border-radius: inherit;
+                @include transition-all;
+                &.pl-progress-bar-percent-normal {
+                    background-color: $color-primary;
+                }
+                &.pl-progress-bar-percent-success {
+                    background-color: $color-success;
+                }
+                &.pl-progress-bar-percent-error {
+                    background-color: $color-error;
+                }
             }
             .pl-progress-bar-active {
                 position: absolute;
@@ -98,5 +135,9 @@
             }
         }
 
+        .pl-progress-label {
+            width: 60px;
+            text-align: right;
+        }
     }
 </style>
