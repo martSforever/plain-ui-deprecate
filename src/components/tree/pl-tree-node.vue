fd173fc78ffa866a55c7fe72810c2b1466131106
martsforever <martsforever@aliyun.com>
2019-02-10 09:22:32 +0800

    add TreeMixin

--- src/components/tree/pl-tree-node.vue
+++ src/components/tree/pl-tree-node.vue
@@ -35,18 +35,14 @@
 <script>
     import PlIcon from "../icon/pl-icon";
     import PlCollapseTransition from "../collapse/pl-collapse-transition";
+    import {TreeMixin} from "./index";
 
     export default {
         name: "pl-tree-node",
         components: {PlCollapseTransition, PlIcon},
+        mixins: [TreeMixin],
         props: {
-            data: {type: Object, default: () => []},                                //渲染的数据
-            labelKey: {type: String, required: true},                               //显示的文本key
-            childrenKey: {type: String, required: true},                            //子树渲染数据的key
-            autoClose: {type: Boolean},                                             //打开节点之后是否关闭兄弟节点
-            emptyText: {type: String, default: '无'},                                //无内容时显示的文本
-            toggleOnClickContent: {type: Boolean, default: true},                   //是否点击节点内容的时候打开|关闭节点
-            initializedAfterOpen: {type: Boolean, default: true},                   //是否在打开的时候才初始化内容
+            data: {type: Object, default: () => ({})},
         },
         data() {
             return {
--- src/components/tree/pl-tree.vue
+++ src/components/tree/pl-tree.vue
@@ -10,6 +10,7 @@
                       :empty-text="emptyText"
                       :toggle-on-click-content="toggleOnClickContent"
                       :initialized-after-open="initializedAfterOpen"
+                      :checkbox="checkbox"
                       @open="val=>$emit('open',val)"
                       @close="val=>$emit('close',val)"
                       @click="val=>$emit('click',val)"
@@ -19,19 +20,12 @@
 
 <script>
     import PlTreeNode from "./pl-tree-node";
+    import {TreeMixin} from "./index";
 
     export default {
         name: "pl-tree",
         components: {PlTreeNode},
-        props: {
-            data: {type: Array, default: () => []},                                 //渲染的数据
-            labelKey: {type: String, required: true},                               //显示的文本key
-            childrenKey: {type: String, required: true},                            //子树渲染数据的key
-            autoClose: {type: Boolean},                                             //打开节点之后是否关闭兄弟节点
-            emptyText: {type: String, default: '无'},                                //无内容时显示的文本
-            toggleOnClickContent: {type: Boolean, default: true},                   //是否点击节点内容的时候打开|关闭节点
-            initializedAfterOpen: {type: Boolean, default: true},                   //是否在打开的时候才初始化内容，如果是想通过open打开特定节点，这里需要设置为false，就是初始化的时候就初始化所有节点，才可以通过open来打开特定节点，否则只能从父节点一层一层打开，到特定的节点
-        },
+        mixins: [TreeMixin],
         methods: {
             p_childToggle(child) {
                 if (!this.autoClose) return
