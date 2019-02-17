ea2b186732cdd5ab4f50c99bf04eb64d68cb1434
martsforever <martsforever@aliyun.com>
2019-02-17 13:51:13 +0800

    complete color opacity silder style

--- src/components/color-picker/pl-color-opacity-slider.vue
+++ src/components/color-picker/pl-color-opacity-slider.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="pl-color-opacity-slider" :style="styles">
+        <div class="pl-color-opacity-slider-shadow" :style="shadowStyles"></div>
     </div>
 </template>
 
@@ -10,8 +11,18 @@
             color: {},
             opacity: {},
         },
+        data() {
+            return {
+                backgroundImage: require('src/asserts/opacity.png')
+            }
+        },
         computed: {
             styles() {
+                return {
+                    backgroundImage: `url(${this.backgroundImage})`
+                }
+            },
+            shadowStyles() {
                 return {
                     background: `linear-gradient(to left, ${this.color}, #ffffff00)`,
                 }
@@ -26,5 +37,9 @@
         height: 10px;
         position: relative;
         cursor: pointer;
+        background-size: 10px 10px;
+        .pl-color-opacity-slider-shadow {
+            height: 100%;
+        }
     }
 </style>
---
+++ src/components/color-picker/pl-color.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pl-color">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pl-color"
+    }
+</script>
+
+<style lang="scss">
+    .pl-color {
+
+    }
+</style>
