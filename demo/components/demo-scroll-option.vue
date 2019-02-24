82f59f24af5bc0e11bad86315dd9f8746001a786
martsforever <martsforever@aliyun.com>
2019-02-24 22:26:16 +0800

    complete scroll option

--- demo/components/demo-scroll-option.vue
+++ demo/components/demo-scroll-option.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="demo-scroll-option">
-        <div class="demo-scroll-option-box">
-            <demo-row>
-                scrollTop:{{scrollTop}}
-            </demo-row>
-            <link-scroll-option :data="data" labelKey="name" valueKey="value" @scroll="val=>scrollTop = val"/>
-        </div>
+        <demo-row title="基本用法">
+            <link-scroll-option :data="data" labelKey="name" valueKey="value" @scroll="val=>scrollTop = val" v-model="scrollVal1"/>
+            &nbsp;
+            <link-scroll-option :data="data" labelKey="name" valueKey="value" @scroll="val=>scrollTop = val" v-model="scrollVal1"/>
+        </demo-row>
+
     </div>
 </template>
 
@@ -15,19 +15,20 @@
         data() {
             return {
                 scrollTop: 0,
+                scrollVal1: 'shanxi',
                 data: [
-                    {name: '张三', value: 'zhangsan'},
-                    {name: '李四', value: 'lisi'},
-                    {name: '王五', value: 'wangwu'},
-                    {name: '张三', value: 'zhangsan'},
-                    {name: '李四', value: 'lisi'},
-                    {name: '王五', value: 'wangwu'},
-                    {name: '张三', value: 'zhangsan'},
-                    {name: '李四', value: 'lisi'},
-                    {name: '王五', value: 'wangwu'},
-                    {name: '张三', value: 'zhangsan'},
-                    {name: '李四', value: 'lisi'},
-                    {name: '王五', value: 'wangwu'},
+                    {name: '广东', value: 'guangdong'},
+                    {name: '上海', value: 'shanghai'},
+                    {name: '北京', value: 'beijing'},
+                    {name: '湖南', value: 'hunan'},
+                    {name: '湖北', value: 'hubei'},
+                    {name: '江西', value: 'jiangxi'},
+                    {name: '山西', value: 'shanxi'},
+                    {name: '天津', value: 'tianjin'},
+                    {name: '贵州', value: 'guizhou'},
+                    {name: '福建', value: 'fujian'},
+                    {name: '广西', value: 'guangxi'},
+                    {name: '山东', value: 'shandong'},
                 ],
             }
         },
--- src/components/scroll-option/pl-scroll-option.vue
+++ src/components/scroll-option/pl-scroll-option.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="pl-scroll-option" :style="styles">
         <div class="pl-scroll-option-wrapper" @scroll="p_scroll" ref="wrapper">
-            <div class="pl-scroll-option-item" v-for="(item) in [1,2,3]" :key="`_${item}`" :style="itemStyles"></div>
-            <div class="pl-scroll-option-item"
-                 v-for="(item,index) in data"
-                 :key="index"
-                 :style="itemStyles"
-                 @click="p_click(item,index)">
-                {{index}},{{!!labelKey ? item[labelKey] : item}}
-            </div>
-            <div class="pl-scroll-option-item" v-for="(item) in [1,2,3]" :key="`${item}_`" :style="itemStyles"></div>
+            <pl-scroll-option-item v-for="(item) in externalData" :key="`top_${item}`"/>
+            <pl-scroll-option-item
+                    v-for="(item,index) in data"
+                    :key="index"
+
+                    :label-key="labelKey"
+                    :value-key="valueKey"
+                    :item-num="itemNum"
+                    :item-height="itemHeight"
+                    :index="index"
+                    :scroll-top="scrollTop"
+                    @click="p_click(item,index)">
+                {{!!labelKey ? item[labelKey] : item}}
+            </pl-scroll-option-item>
+            <pl-scroll-option-item v-for="(item) in externalData" :key="`bottom_${item}`"/>
         </div>
         <div class="pl-scroll-option-shadow-top" :style="{height:`${itemNum*itemHeight}px`}"></div>
         <div class="pl-scroll-option-shadow-bottom" :style="{height:`${itemNum*itemHeight}px`}"></div>
@@ -18,35 +24,55 @@
 
 <script>
 
+    import PlScrollOptionItem from "./pl-scroll-option-item";
+
     const scroll = require('scroll');
 
     export default {
         name: "pl-scroll-option",
+        components: {PlScrollOptionItem},
         props: {
+            value: {},
             data: {type: Array, required: true},
             labelKey: {type: String},
             valueKey: {type: String},
             itemHeight: {type: Number, default: 24},
             itemNum: {type: Number, default: 3},
+            width: {type: Number, default: 100},
         },
         data() {
             return {
+                p_value: this.value,
                 scrollTop: 0,
                 timer: null,
                 listenScroll: true,
                 p_index: 0,
             }
         },
-        computed: {
-            styles() {
-                return {
-                    height: `${this.itemHeight * ((this.itemNum * 2) + 1)}px`
+        watch: {
+            value(val) {
+                if (val !== this.p_value) {
+                    this.p_updateByValue()
                 }
             },
-            itemStyles() {
-                return {
-                    height: `${this.itemHeight}px`
+        },
+        mounted() {
+            if (this.value != null) this.p_updateByValue()
+        },
+        computed: {
+            styles() {
+                const styles = {height: `${this.itemHeight * ((this.itemNum * 2) + 1)}px`}
+                this.width != null && (styles.width = this.width + 'px')
+                return styles
+            },
+            externalData() {
+                const ret = []
+                let i = 0
+                while (i < this.itemNum) {
+                    ret.push(i)
+                    i++
                 }
+                return ret
             },
         },
         methods: {
@@ -87,13 +113,29 @@
             },
             p_updateIndex() {
                 this.p_index = Math.floor((this.scrollTop / this.itemHeight))
-                console.log(this.p_index)
+                this.p_emitValue()
             },
             p_scrollTo(index) {
                 this.listenScroll = false
                 this.p_index = index
+                this.p_emitValue();
                 scroll.top(this.$refs.wrapper, (index) * this.itemHeight);
                 setTimeout((() => this.listenScroll = true), 300)
+            },
+            p_emitValue() {
+                this.p_value = this.p_getValue(this.data[this.p_index])
+                this.$emit('input', this.p_value)
+            },
+            p_updateByValue() {
+                for (let i = 0; i < this.data.length; i++) {
+                    const item = this.data[i];
+                    if (this.p_getValue(item) === this.value) {
+                        scroll.top(this.$refs.wrapper, (i) * this.itemHeight);
+                    }
+                }
+            },
+            p_getValue(item) {
+                return !!this.valueKey ? item[this.valueKey] : item
             },
         }
     }
@@ -102,11 +144,11 @@
 <style lang="scss">
     .pl-scroll-option {
         @include public-style;
-        width: 100%;
         height: 100%;
         overflow: hidden;
         border: solid 1px #ddd;
         position: relative;
+        display: inline-block;
         .pl-scroll-option-wrapper {
             width: calc(100% + 17px);
             height: 100%;
