e98a21559c6664db0abca8b91ffcc9ab357a59d2
韦胜健 <martsforever@aliyun.com>
2019-03-07 19:08:31 +0800

    adjust tooltip

--- src/components/tooltip/pl-tooltip-text.vue
+++ src/components/tooltip/pl-tooltip-text.vue
@@ -58,7 +58,7 @@
     .pl-tooltip-text {
         display: block;
         height: fit-content;
-        padding: 6px 0;
+        padding: 3px 0;
         .pl-tooltip-text-content {
             outline: none;
             overflow: hidden;
