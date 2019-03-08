3ad83b05278ff64f7fb249a7683ff5b66ca4e712
martsforever <martsforever@aliyun.com>
2019-03-08 21:37:50 +0800

    save demo code

--- demo/components/demo-date.vue
+++ demo/components/demo-date.vue
@@ -2,7 +2,7 @@
     <div class="demo-date">
         <!--<demo-row :title="`普通用法，选择日期:${val[0]}`">
             <link-date v-model="val[0]"/>
-        </demo-row>-->
+        </demo-row>
         <demo-row title="三种视图">
             <demo-row-item title="年">
                 <link-date v-model="val[1]" view="year"/>
@@ -24,9 +24,6 @@
             <demo-row-item title="日">
                 <link-date v-model="val[3]" view="date" max="2020-05-05" min="2018-04-01"/>
             </demo-row-item>
-        </demo-row>
-        <!--<demo-row title="最大最小时间,max=2020-05-05，min=2018-04-01">
-            <link-date v-model="val[4]" max="2020-05-05" min="2018-04-01"/>
         </demo-row>
         <demo-row :title="`值以及显示值格式化:${val[5]}`">
             <link-date v-model="val[5]" valueFormat="YYYYMMDD" displayFormat="YYYY年MM月DD日"/>
