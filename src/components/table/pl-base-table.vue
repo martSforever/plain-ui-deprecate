cfcd2cb725d63041a207749fa27c44f9f140772f
韦胜健 <martsforever@aliyun.com>
2019-03-12 19:48:30 +0800

    remove log code

--- src/components/table/pl-base-table.vue
+++ src/components/table/pl-base-table.vue
@@ -204,11 +204,7 @@
                 /*首次加载还没有挂载mounted的时候，columns应该是空的，收集的bodyColumns也是空的，直接返回空数组*/
                 if (cols.length === 0) return cols
                 /*计算所有列的总宽度*/
-                const totalColumnWidth = cols.reduce((ret, item) => {
-                    console.log(item)
-                    return ret + item.width
-                }, 0)
-                console.log(totalColumnWidth, this.p_tableWidth)
+                const totalColumnWidth = cols.reduce((ret, item) => ret + item.width, 0)
                 /*如果所有列的总宽度小于表格宽度，按照列的权重给列分配剩下的宽度*/
                 if (totalColumnWidth < this.p_tableWidth) {
                     /*额外多出来的宽度*/
