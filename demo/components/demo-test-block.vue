f2d75562017de9fe2599f0c76d479885e47e078a
martsforever <martsforever@aliyun.com>
2019-03-13 00:18:52 +0800

    complete demo block

--- demo/components/demo-test-block.vue
+++ demo/components/demo-test-block.vue
@@ -1,13 +1,10 @@
 <template>
     <div class="demo-test-block">
-        demo-test-block
         <demo-row>
             <demo-block>
                 <link-button label="hello"/>
             </demo-block>
         </demo-row>
-        <div class="aaa"></div>
-
     </div>
 </template>
 
@@ -19,16 +16,4 @@
 
 <style lang="scss">
 
-    body {
-        --aaa: #ddd;
-    }
-
-    $color-aaa: var(--aaa);
-
-    .aaa {
-        /*background-color: rgba($color-aaa, 0.2);*/
-        background-color: rgba(var(--aaa), 0.2);
-        height: 100px;
-        width: 100px;
-    }
 </style>
--- demo/demo-block.vue
+++ demo/demo-block.vue
@@ -10,8 +10,9 @@
         props: {
             file: {type: String, default: 'button.md'},
         },
-        created() {
-            this.p_getBlocks()
+        async created() {
+            const blocks = await this.p_getBlocks()
+            console.log(blocks)
         },
         methods: {
             async p_decodeFile() {
@@ -25,7 +26,30 @@
             async p_getBlocks() {
                 const blocksText = await  this.p_decodeFile()
                 const blocks = blocksText.split("---")
-                console.log(blocks)
+                return blocks.map(block => this.p_decodeBlock(block))
+            },
+            p_decodeBlock(block) {
+                let types = ['html', 'js', 'scss']
+                const result = types.reduce((ret, item) => {
+                    const {content, block: newBlock} = this.p_decodeBlockType(block, item)
+                    block = newBlock
+                    ret[item] = content
+                    return ret
+                }, {})
+                result.md = block
+                return result
+            },
+            p_decodeBlockType(block, codeType) {
+                let type = '```' + codeType
+                let startIndex = block.indexOf(type), endIndex, content
+                if (startIndex > -1) {
+                    endIndex = block.indexOf('```', startIndex + type.length) + 3
+                    content = block.substring(startIndex, endIndex)
+                    block = block.replace(content, '')
+                    content = content.replace(type, '')
+                    content = content.replace('```', '')
+                }
+                return {content, block}
             },
         }
     }
