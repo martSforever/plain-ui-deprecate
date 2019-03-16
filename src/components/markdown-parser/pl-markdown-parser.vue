9feaa6322885a24a606e104b9ee110eb3b0feb44
martsforever <martsforever@aliyun.com>
2019-03-16 22:22:46 +0800

    parser nav basic styles

--- src/components/markdown-parser/pl-markdown-parser.vue
+++ src/components/markdown-parser/pl-markdown-parser.vue
@@ -2,9 +2,31 @@
     <div class="pl-markdown-parser">
         <pl-scroll class="pl-markdown-parser-scroll" @scroll="e=>$emit('scroll',e)">
             <div class="pl-markdown-parser-wrapper">
-                <div v-for="(item,index) in demos" :key="index" :data="item">
-                    <pl-markdown-parser-item :data="item" v-if="item.isDemo" :left-width="leftWidth" :screen-height="screenHeight" @showInDialog="p_showInDialog"/>
-                    <pl-markdown :value="item.md" v-else/>
+                <div class="pl-markdown-parser-demo-wrapper">
+                    <div v-for="(item,index) in demos" :key="index" :data="item">
+                        <pl-markdown-parser-item :data="item"
+                                                 v-if="item.isDemo"
+                                                 :left-width="leftWidth"
+                                                 :screen-height="screenHeight"
+                                                 @showInDialog="p_showInDialog"
+                                                 @created="p_itemCreated"
+                                                 @destroyed="p_itemDestroyed"/>
+                        <pl-markdown :value="item.md" v-else/>
+                    </div>
+                </div>
+                <div class="pl-markdown-parser-nav" :style="navStyles" ref="nav">
+                    <div class="pl-markdown-parser-nav-item"
+                         :class="{'pl-markdown-parser-nav-item-active':position === index}"
+                         v-for="(item,index) in navs"
+                         :key="index"
+                         @click="position = index">
+                        <span>{{item.title}}</span>
+                    </div>
+                    <div class="pl-markdown-parser-nav-slider">
+                        <div class="pl-markdown-parser-nav-slider-indicator" :style="{top:`${position*28+9}px`}">
+                            <pl-icon icon="pl-radio-on"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </pl-scroll>
@@ -22,10 +44,13 @@
     import PlMarkdownParserItemCloser from "./pl-markdown-parser-item-closer";
     import PlScroll from "../scroll/pl-scroll";
     import PlDialog from "../dialog/pl-dialog";
+    import PlIcon from "../icon/pl-icon";
+    import {MountedMixin} from "../../mixin/component-mixin";
 
     export default {
         name: "pl-markdown-parser",
-        components: {PlDialog, PlScroll, PlMarkdownParserItemCloser, PlMarkdown, PlMarkdownParserItem},
+        components: {PlIcon, PlDialog, PlScroll, PlMarkdownParserItemCloser, PlMarkdown, PlMarkdownParserItem},
+        mixins: [MountedMixin],
         props: {
             value: {},
             leftWidth: {type: Number, default: 300},
@@ -44,7 +69,23 @@
                 screenHeight: window.screen.height,
                 dialogShow: false,
                 code: null,
+                position: 0,
             }
+        },
+        computed: {
+            navs() {
+                return this.demos.filter(item => item.isDemo)
+            },
+            navStyles() {
+                if (!this.p_mounted) return null
+                const {left, top} = this.$refs.nav.getBoundingClientRect()
+                return {
+                    position: 'fixed',
+                    top: `${top}px`,
+                    left: `${left}px`,
+                    right: null,
+                }
+            },
         },
         methods: {
             /*
@@ -141,17 +182,61 @@
 </script>
 
 <style lang="scss">
+    $nav-padding: 12px;
     .pl-markdown-parser {
         @include public-style;
         height: 100%;
         width: 100%;
         position: relative;
         overflow: hidden;
-
         .pl-markdown-parser-wrapper {
             width: 800px;
             padding: 12px;
             box-sizing: border-box;
+            position: relative;
+            .pl-markdown-parser-nav {
+                position: absolute;
+                top: $nav-padding;
+                width: 200px;
+                right: -200px - $nav-padding*3;
+                padding: 0 12px;
+                user-select: none;
+                .pl-markdown-parser-nav-item {
+                    width: 100%;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    font-size: $nav-padding;
+                    cursor: pointer;
+                    &:not(:first-child) {
+                        margin-top: 12px;
+                    }
+                    &:hover {
+                        color: $color-primary;
+                    }
+                    &.pl-markdown-parser-nav-item-active {
+                        color: $color-primary;
+                    }
+                }
+                .pl-markdown-parser-nav-slider {
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    width: 1px;
+                    background-color: #c5def7;
+                    .pl-markdown-parser-nav-slider-indicator {
+                        @include transition-all-cubic-bezier;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        width: 1px;
+                        height: 1px;
+                        color: $color-primary;
+                        position: absolute;
+                    }
+                }
+            }
         }
 
     }
