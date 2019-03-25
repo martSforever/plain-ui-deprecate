16556ef01bf5103133584694e1434b2b5e88fbdd
韦胜健 <martsforever@aliyun.com>
2019-03-25 21:08:53 +0800

    adjust context menu

--- src/components/context-menu/ContextMenu.js
+++ src/components/context-menu/ContextMenu.js
@@ -6,21 +6,17 @@ class ContextMenu {
 
     constructor(Vue) {
         this.Vue = Vue
-        this.init()
+        this.Vue.prototype.$nextTick(() => {
+            this.instance = new this.Vue(PlContextMenu).$mount()
+            document.body.appendChild(this.instance.$el)
+        })
     }
 
     async pick({data, labelKey, valueKey, value, multiple, width, height, el, event} = {}) {
         return new Promise((rs) => {
             Object.assign(this.instance, {data, labelKey, valueKey, value, multiple, width, height, el, event}, {onConfirm: rs})
-            this.Vue.prototype.$nextTick(() => {
-                this.instance.show = true
-            })
+            this.instance.show = true
         })
-    }
-
-    init() {
-        this.instance = new this.Vue(PlContextMenu).$mount()
-        document.body.appendChild(this.instance.$el)
     }
 }
 
