407ae91b56dd32e065479934780a32173d621e8c
韦胜健 <martsforever@aliyun.com>
2019-04-01 10:27:05 +0800

    complete  nav event system

--- src/components/nav/pl-nav-page.vue
+++ src/components/nav/pl-nav-page.vue
@@ -22,16 +22,20 @@
         },
         data() {
             const pages = this.$plain.$dom.findComponentUpward(this, 'pl-nav-pages')
+            const events = []
+            const that = this
             return {
+                events,
+                pages,
                 nav: {
                     push: (...args) => pages.push(...args),
                     back: (...args) => pages.back(...args),
                     redirect: (...args) => pages.redirect(...args),
                     backOff: (...args) => pages.backOff(...args),
-                    on: (...args) => pages.on(...args),
-                    once: (...args) => pages.once(...args),
-                    off: (...args) => pages.off(...args),
-                    emit: (...args) => pages.emit(...args),
+                    on: (...args) => that.on(...args),
+                    once: (...args) => that.once(...args),
+                    off: (...args) => that.off(...args),
+                    emit: (...args) => that.emit(...args),
                 }
             }
         },
@@ -56,8 +60,60 @@
                     return this.$refs.page.onBack(data)
                 }
             },
-        }
 
+            /**
+             * 监听事件
+             * @author  韦胜健
+             * @date    2019/3/19 18:50
+             */
+            on(event, callback) {
+                this.events.push({event, callback})
+                this.pages.on(event, callback)
+            },
+
+            /**
+             * 只监听一次事件
+             * @author  韦胜健
+             * @date    2019/3/19 18:51
+             */
+            once(event, callback) {
+                this.events.push({event, callback})
+                this.pages.once(event, callback)
+            },
+
+            /**
+             * 移除事件
+             * @author  韦胜健
+             * @date    2019/3/19 18:51
+             */
+            off(event, callback) {
+                for (let i = 0; i < this.events.length; i++) {
+                    const {event: e, callback: c} = this.events[i];
+                    if (event !== e) continue
+                    if (callback == null || callback === c) {
+                        this.events.splice(i, 1)
+                        this.pages.off(event, callback)
+                        i--
+                        return true
+                    }
+                }
+                return false
+            },
+
+            /**
+             * 派发事件
+             * @author  韦胜健
+             * @date    2019/3/19 18:51
+             */
+            emit(event, param, global) {
+                this.pages.emit(event, param, global)
+            },
+        },
+        beforeDestroy() {
+            /*页面销毁*/
+            if (!this.pages) return
+            this.events.forEach(({event, callback}) => this.off(event, callback))
+        },
     }
 </script>
 
