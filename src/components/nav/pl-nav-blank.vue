c8b7005c66aea04dca1a6eaac3a076e5c361b689
韦胜健 <martsforever@aliyun.com>
2019-04-01 19:27:10 +0800

    new

---
+++ src/components/nav/pl-nav-blank.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pl-nav-blank">
+        pl-nav-blank
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pl-nav-blank"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
