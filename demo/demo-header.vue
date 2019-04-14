f2212e3344e32a4cc0f52a16a99154a891f4d2cb
martsforever <martsforever@aliyun.com>
2019-04-14 22:22:55 +0800

    add black theme

--- demo/demo-header.vue
+++ demo/demo-header.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="demo-header">
         <span>Plain-UI DEMO</span>
-            <link-button-group>
-                <link-button box-type="line" label="改变主题："/>
-                <link-button v-for="(item,index) in ['default','black','custom']" :label="item" :key="index" @click="$plain.changeTheme(item)"/>
-            </link-button-group>
+        <link-button-group>
+            <link-button box-type="line" label="改变主题："/>
+            <link-button v-for="(item,index) in ['default','black','custom']" :label="item" :key="index" @click="$plain.changeTheme(item)"/>
+        </link-button-group>
     </div>
 </template>
 
@@ -17,24 +17,28 @@
 </script>
 
 <style lang="scss">
-    .demo-header {
-        height: 64px;
-        width: 100%;
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 12px;
-        box-sizing: border-box;
-        &::after {
-            box-shadow: 0 -5px 20px #ddd;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            top: 0;
-            content: '';
-            z-index: -1;
+    @include themeify {
+        .demo-header {
+            height: 64px;
+            width: 100%;
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 12px;
+            box-sizing: border-box;
+            background-color: $p-color-normal-background;
+            color: $p-color-normal-title;
+            &::after {
+                box-shadow: 0 -5px 20px #ddd;
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                top: 0;
+                content: '';
+                z-index: -1;
+            }
         }
     }
 </style>
